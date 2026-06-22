d372b5dbf951132f6c828c32e9692418954acdc1
Aaron Wilson <65009367+slochewie@users.noreply.github.com>
2026-06-22 03:12:06 -0700

    Update apps.h

--- main/apps/apps.h
+++ main/apps/apps.h
@@ -4,10 +4,7 @@
  * SPDX-License-Identifier: MIT
  */
 #pragma once
-#include "app_template/app_template.h"
 #include "app_launcher/app_launcher.h"
-#include "app_watch_face/app_watch_face.h"
 #include "app_counter/app_counter.h"
 #include "app_configure/app_configure.h"
 #include "app_setup/app_setup.h"
-#include "apps/app_alarm_clock/app_alarm_clock.h"
